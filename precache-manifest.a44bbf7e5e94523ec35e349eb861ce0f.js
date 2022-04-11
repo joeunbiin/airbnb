3ea8c01dd781c930a555c91cbf8c1b16e22f8bc5
joeunbiin <102141487+joeunbiin@users.noreply.github.com>
2022-04-12 01:22:46 +0900

    ver.1.8

---
+++ precache-manifest.a44bbf7e5e94523ec35e349eb861ce0f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "98ebeb9274530f8fc8f9",
+    "url": "/static/css/main.61fe39b7.chunk.css"
+  },
+  {
+    "revision": "98ebeb9274530f8fc8f9",
+    "url": "/static/js/main.0998f89a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "ab10bd9e16ee1b70b283",
+    "url": "/static/css/2.7f494059.chunk.css"
+  },
+  {
+    "revision": "ab10bd9e16ee1b70b283",
+    "url": "/static/js/2.a42a3bd2.chunk.js"
+  },
+  {
+    "revision": "307e085c39d9249439b7acf01b3b521d",
+    "url": "/index.html"
+  }
+];
